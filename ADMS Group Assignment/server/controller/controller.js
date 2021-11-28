6df22d3529889e8962eb9562e5a88c2554baa8d8
w3schools-test <test@w3schools.com>
2021-11-28 16:16:05 +0530

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=8315bb4741b363016d2d&state=11d007ee-fd04-4f54-8396-f1beda448420

---
+++ ADMS Group Assignment/server/controller/controller.js
@@ -0,0 +1,106 @@
+var Userdb = require('../model/model');
+
+// create and save new employee
+exports.create = (req,res)=>{
+    // validate request
+    if(!req.body){
+        res.status(400).send({ message : "Content can not be emtpy!"});
+        return;
+    }
+
+    // new employee
+    const user = new Userdb({
+        name : req.body.name,
+        email : req.body.email,
+        gender: req.body.gender,
+        status : req.body.status
+    })
+
+    // save employee in the database
+    user
+        .save(user)
+        .then(data => {
+            //res.send(data)
+            res.redirect('/add-user');
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message : err.message || "Some error occurred while creating a create operation"
+            });
+        });
+
+}
+
+// retrieve and return all employees/ retrive and return a single employee
+exports.find = (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        Userdb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found employee with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving employee with id " + id})
+            })
+
+    }else{
+        Userdb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving employee information" })
+            })
+    }
+
+    
+}
+
+// Update a new idetified employee by employee id
+exports.update = (req, res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({ message : "Data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Update user with ${id}. Maybe employee not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({ message : "Error Update employee information"})
+        })
+}
+
+// Delete a employee with specified employee id in the request
+exports.delete = (req, res)=>{
+    const id = req.params.id;
+
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message : "Employee was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: "Could not delete employee with id=" + id
+            });
+        });
+}
